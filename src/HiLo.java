41427cb9c573995d9bb30b5ad88e9b7495177e36
jose-marquez89 <jlmarquez89@gmail.com>
2022-01-30 08:55:20 -0600

    replaces spaces with tabs

--- src/HiLo.java
+++ src/HiLo.java
@@ -4,27 +4,27 @@ public class HiLo {
 
 	public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-		String playAgain = "";
-		
-		do {
-			int theNumber = (int)(Math.random() * 10 + 1);
-			int guess = 0;
-			while (guess != theNumber) {
-				System.out.println("Guess a number between 1 and 10:");
-				guess = scan.nextInt();
-				
-				if (guess < theNumber)
-					System.out.println(guess + " is too low. Try again.");
-				else if (guess > theNumber)
-					System.out.println(guess + " is too high. Try again.");
-				else
-					System.out.println(guess + " is correct. You win!");
-			}
-			System.out.println("Would you like to play again?");
-			playAgain = scan.next();
-		} while (playAgain.equalsIgnoreCase("y"));
-		
-		// close scan because eclipse said so
-		scan.close();
-	}
+        String playAgain = "";
+
+        do {
+            int theNumber = (int)(Math.random() * 10 + 1);
+            int guess = 0;
+            while (guess != theNumber) {
+                System.out.println("Guess a number between 1 and 10:");
+                guess = scan.nextInt();
+
+                if (guess < theNumber)
+                    System.out.println(guess + " is too low. Try again.");
+                else if (guess > theNumber)
+                    System.out.println(guess + " is too high. Try again.");
+                else
+                    System.out.println(guess + " is correct. You win!");
+            }
+            System.out.println("Would you like to play again?");
+            playAgain = scan.next();
+        } while (playAgain.equalsIgnoreCase("y"));
+
+        // close scan because eclipse said so
+        scan.close();
+    }
 }
