f63b2aa823558a42eb13ef8ca23e51c3370d2c98
jose-marquez89 <jlmarquez89@gmail.com>
2022-01-29 19:21:20 -0600

    adds initial source files

---
+++ src/HiLo.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class HiLo {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int theNumber = (int)(Math.random() * 100 + 1);
+		
+		int guess = 0;
+		
+		while (guess != theNumber) {
+			System.out.println("Guess a number between 1 and 100:");
+			guess = scan.nextInt();
+			
+			if (guess < theNumber)
+				System.out.println(guess + " is too low. Try again.");
+			else if (guess > theNumber)
+				System.out.println(guess + " is too high. Try again.");
+			else
+				System.out.println(guess + " is correct. You win!");
+		}
+	}
+}
